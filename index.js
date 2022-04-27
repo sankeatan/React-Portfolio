f77f3140067aeef74a56d13302a62cea3ca88a0d
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-04-27 15:48:50 -0400

    changed filed structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
+import App from './src/App';
 import './index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
