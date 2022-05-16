6dfd21e34e18e51d2452e3eec87eb4a71a433b0b
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-05-16 11:01:22 -0400

    did some work to make it more mobile responsive

--- src/Layout.js
+++ src/Layout.js
@@ -4,13 +4,11 @@ import './Layout.scss'
 
 export default function Layout () {
     return (
-        <>
             <div className='outlet'>
         <Outlet/>
-            </div >
             <div className='navBar'>
         <NavBar />
             </div>
-        </>
+            </div >
     )
 }
