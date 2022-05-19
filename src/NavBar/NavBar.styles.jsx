332241cc532400f6c8bd6698544dccc1d30cf147
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-05-19 19:47:03 -0400

    migrating navbar to styled div

--- src/NavBar/NavBar.styles.jsx
+++ src/NavBar/NavBar.styles.jsx
@@ -10,3 +10,75 @@ const shadowPulse = keyframes`
   }
 }
 `;
+
+export const NavContainer = styled.div`
+{
+    display:flex;
+    justify-content:center;
+    background-color: #FED766;
+    height: 100vh;
+    width: 20rem;
+  }
+}
+`;
+
+export const Portrait = styled.img`
+{
+  position: absolute;
+  height: 200px;
+	width: 200px;
+	background-size: cover;
+  background-position: center;
+	border-radius: 100%;
+	box-shadow: 5px 5px 15px #000001e5;
+}
+`;
+
+export const Name = styled.h1`
+{
+  position: absolute;
+  top: 13rem;
+  font-family: 'Staatliches', cursive;
+}
+`;
+
+export const CustomIconContainer = styled.div`
+{
+  position: absolute;
+  flex-direction: row;
+  top: 16rem;
+}`;
+
+export const CustomIcon = styled.a`
+{
+  font-family: 'Space Grotesk', sans-serif;
+  font-weight: 300;
+  font-padding: 0px;
+  font-margin: 0px;
+}
+`;
+
+export const ButtonContainer = styled.div`
+{
+    position: absolute;
+    display:flex;
+    flex-direction:column;
+    top: 18rem;
+}`;
+
+export const CustomButton = styled.div`
+{
+  background-color: #86BBD8;
+	color: #000000;
+	text-decoration: none;
+	display: block;
+	font-family: 'Space Grotesk', sans-serif;
+	font-size: 1.5rem;
+	font-weight: 300;
+	padding: 1rem;
+	margin: 1rem;
+	border-radius: 10px;
+	width: 200px;
+}
+`;
+
