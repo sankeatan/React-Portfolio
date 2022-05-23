0f2cf112f3307bc2027f028875eee58b74c265f4
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-05-23 11:28:06 -0400

    stylized the about view

---
+++ src/views/about/index.styles.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AboutMe =styled.div 
+`{
+    font-size: 1.5rem;
+    padding-top: 4rem;
+    padding-right: 10rem;
+    margin-left: 4rem;
+    height: 100vh;
+    overflow: scroll;
+}`;
